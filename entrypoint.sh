fb140e038f0b601790dfd5f03209adea68f69c97
Brandon Compertore <Brandon@Compertore.com>
2020-05-02 13:05:20 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@
 set -e
 
 cd "$INPUT_IMAGE_DIR"
-ls
 
 echo "$INPUT_SERVICE_KEY" | base64 --decode > "$HOME"/gcloud.json
 
@@ -22,6 +21,7 @@ fi
 gcloud auth activate-service-account --key-file="$HOME"/gcloud.json --project "$INPUT_PROJECT"
 gcloud auth configure-docker
 
+docker build --tag "$INPUT_IMAGE" .
 docker push "$INPUT_IMAGE"
 
 gcloud beta run deploy "$INPUT_SERVICE" \
